9be3d54aad71b7baa3758c683e6a8ef5b5314cb1
banana <ahmadjd94@gmail.com>
2019-12-31 01:00:38 +0200

    fix tests add, cyclic sort, bug in binary search

--- search/binary_search.py
+++ search/binary_search.py
@@ -4,11 +4,17 @@ from math import floor
 def binary_search(lst: list, target: int):
     l = len(lst)
     d = floor(l/2)
-    if l >= 1:
-        if target > lst[d]:
+
+    if l == 1:
+        return target == lst[0]
+    elif l == 0:
+        return False
+    else:
+        if target == lst[d]:
+            return True
+        elif target > lst[d]:
             return binary_search(lst[d:], target)
         elif target < lst[d]:
             return binary_search(lst[:d], target)
-        elif target == lst[d]:
-            return True
+
     return False
--- tests/search/binary_search.py
+++ tests/search/binary_search.py
@@ -2,9 +2,9 @@ from search import binary_search
 
 
 def test_binary_search():
-    a = [i for i in range(2*10)]
+    a = [i for i in range(2**10)]
     assert binary_search(a,5) is True
     assert binary_search(a,321) is True
     assert binary_search(a,-1) is False
     assert binary_search(a,0) is True
-    assert binary_search(a,321) is False  #todo failing test case
+    assert binary_search(a,2**11) is False  #todo failing test case
